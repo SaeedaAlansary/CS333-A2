da66e5b294afa5c371209be06287a539ae9d5073
DanahMk <143724527+DanahMk@users.noreply.github.com>
2024-11-30 16:44:14 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Nationalities</title>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+  >
+</head>
+<body>
+  <main class="container">
+    <h1>Statistics of Students Nationalities</h1>
+    <table>
+      <thead>
+        <tr>
+          <th><b>Year</b></th>
+          <th><b>Semester</b></th>
+          <th><b>The Programs</b></th>
+          <th><b>Nationality</b></th>
+          <th><b>Colleges</b></th>
+          <th><b>Number of Students</b></th>
+        </tr>
+      </thead>
+      <tbody>
+</tbody>
+    </table>
+  </main>
+</body>
+</html>
